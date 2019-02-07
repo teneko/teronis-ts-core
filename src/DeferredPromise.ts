2779517e666196a5f6b48a44607f87fb640b7af6
teroneko <giteroneko@gmail.com>
2019-02-07 09:26:08 +0000

    DeferredPromise has now a default generic constraint\r\n Removed IDeferredPromise

--- src/DeferredPromise.ts
+++ src/DeferredPromise.ts
@@ -1,13 +1,9 @@
-export interface IDeferredPromise<T = any> {
-    promise: Promise<T>;
-    resolve: (value: T) => void;
-    reject: (value: any) => void;
-}
-
-export class DeferredPromise<T> {
+export class DeferredPromise<T = any> {
     public promise: Promise<T>;
-    public resolve: ((value: T) => void) | undefined;
-    public reject: ((value: any) => void) | undefined;
+    // @ts-ignore It will be initialized while constructering
+    public resolve: ((value: T) => void);
+    // @ts-ignore It will be initialized while constructering
+    public reject: ((value: any) => void);
 
     constructor() {
         this.promise = new Promise<T>((rs, rj) => {
